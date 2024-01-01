41d3a2a27bbca6b75fc1ab45d38e5ccdb8407fd6
olivierhuynh-web <olivier.huynh@yahoo.fr>
2024-01-01 22:02:43 +0100

    fin de jour 3

--- src/app/page.js
+++ src/app/page.js
@@ -1,50 +1,45 @@
 'use client';
+// Importez les modules nécessaires
 import gsap from 'gsap';
-import React, { useLayoutEffect, useEffect, useRef } from 'react';
+import { TextPlugin } from 'gsap/dist/TextPlugin'; // Importez TextPlugin
+import { useLayoutEffect, useRef } from 'react';
 import styles from './page.module.css';
-import './reset.css';
-import Image from 'next/image';
 
-const useIsomorphicLayoutEffect =
-  typeof window !== 'undefined' ? useLayoutEffect : useEffect;
+gsap.registerPlugin(TextPlugin); // Enregistrez le plugin
 
-export default function index() {
-  const navigate = () => {
-    window.location.href = '/2';
-  };
+export default function Index() {
   const ref = useRef();
-  const container = useRef();
-  useIsomorphicLayoutEffect(() => {
-    let ctx = gsap.context(() => {
-      gsap.fromTo(
-        ref.current,
-        {
-          y: 0,
-        },
-        {
-          y: -50,
-          duration: 0.2,
-          ease: 'bounce.out', // Changez 'power2.inOut' en 'bounce.out'
-          repeat: 3, // Changez -1 en le nombre de fois que vous voulez que l'animation se répète
-          yoyo: true,
-        }
-      );
-      gsap.set(container.current, { backgroundColor: 'yellow' });
-    });
-    return () => ctx.revert();
+
+  useLayoutEffect(() => {
+    // Créez une timeline GSAP pour l'animation du texte
+    const tl = gsap.timeline();
+
+    tl.fromTo(
+      ref.current,
+      {
+        duration: 2,
+        // text: 'This is the new text',
+        ease: 'none',
+      },
+      {
+        duration: 2, // Durée pour chaque lettre
+        text: 'This is the new text lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+        ease: 'power1.inOut',
+        // onComplete: addLetters,
+      }
+    );
+
+    // Revert l'animation à la fin du composant
+    return () => tl.revert();
   }, []);
 
   return (
-    <div className={styles.container} ref={container}>
-      <div ref={ref} className={styles.myH1}>
-        <Image
-          src='/animation_female_vagina_3d_model_c4d_max_obj_fbx_ma_lwo_3ds_3dm_stl_1643693_o-removebg-preview.png' // Chemin vers votre image
-          alt='vagina'
-          width={400} // Largeur de l'image
-          height={300} // Hauteur de l'image
-          onClick={() => navigate()}
-        />{' '}
-      </div>
+    <div className={styles.container}>
+      <span className={styles.text} ref={ref}>
+        {/* Votre texte initial */}
+        {/* Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt... */}
+      </span>
     </div>
   );
 }
