56149f1e382564705d5d582ae72c6d5559b2dca3
olivierhuynh-web <olivier.huynh@yahoo.fr>
2024-02-12 23:29:27 +0100

    map

--- src/app/components/Projects/Projects.js
+++ src/app/components/Projects/Projects.js
@@ -1,24 +1,32 @@
+// ==================== IMPORT BIBLIOTHEQUES ====================
 import { React, useEffect, useRef } from 'react';
-import styles from './Projects.module.scss';
-import CustomFont from '@next/font/local';
-import { Space_Mono } from '@next/font/google';
 import Image from 'next/image';
 import { gsap } from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
+// ===============================================
 
+// ==================== IMPORT ANIMATIONS + DATA ====================
 import { introductionCards, scaleCards, pin } from './animations';
+import data from '../../db/data.json';
 
+// ===============================================
+
+// ==================== IMPORT STYLES ====================
+import styles from './Projects.module.scss';
+import CustomFont from '@next/font/local';
+import { Space_Mono } from '@next/font/google';
 const rightGrotesk = CustomFont({
   src: '../../fonts/PPRightGrotesk-CompactDark.otf',
-  // variable: '--font-cfont',
 });
-
 const spaceMono = Space_Mono({
   subsets: ['latin'],
   weight: ['400'],
 });
+// ===============================================
 
+//! ==================== COMPOSANT ====================
 const Projects = () => {
+  // ==================== SELECTEURS ====================
   const section_2 = useRef(null);
   const section2Wrapper = useRef(null);
   const elementHorizontal0 = useRef(null);
@@ -28,6 +36,7 @@ const Projects = () => {
   const elementHorizontal4 = useRef(null);
   const elementHorizontal5 = useRef(null);
   const cards = document.querySelectorAll(`.${styles.horizontalElement}`);
+  // ===============================================
 
   useEffect(() => {
     gsap.registerPlugin(ScrollTrigger);
@@ -44,158 +53,33 @@ const Projects = () => {
       <section className={styles.section_2} ref={section_2}>
         <div className={styles.section_2__wrapper} ref={section2Wrapper}>
           <div></div>
-          <div className={styles.horizontalElement} ref={elementHorizontal0}>
-            {/* <div className='card'> */}
-            <div className={styles.image_wrapper}>
-              <Image
-                src='https://picsum.photos/500/300/?image=10'
-                width={500}
-                height={300}
-                alt='Picture of the author'
-                className={styles.card_image}
-              />{' '}
+          {data.projects.map((project) => (
+            <div className={styles.horizontalElement} ref={elementHorizontal0}>
+              {/* <div className='card'> */}
+              <div className={styles.image_wrapper}>
+                <Image
+                  src='https://picsum.photos/500/300/?image=10'
+                  width={500}
+                  height={300}
+                  alt='Picture of the author'
+                  className={styles.card_image}
+                />
+              </div>
+              <div className={styles.card_content}>
+                <h3
+                  className={`
+                      ${styles.card_title}
+                       ${rightGrotesk.className}`}
+                >
+                  {project.name}
+                </h3>
+                <p className={spaceMono.className}>
+                  Demo of pixel perfect pure CSS simple responsive card grid
+                  layout
+                </p>
+              </div>
             </div>
-            <div className='card_content'>
-              <h3
-                className={`
-                  ${styles.card_title}
-                   ${rightGrotesk.className}`}
-              >
-                Card Grid Layout
-              </h3>
-              <p className={spaceMono.className}>
-                Demo of pixel perfect pure CSS simple responsive card grid
-                layout
-              </p>
-              <button className='btn card_btn'>Read More</button>
-            </div>
-            {/* </div> */}
-          </div>
-          <div className={styles.horizontalElement} ref={elementHorizontal1}>
-            <div className={styles.image_wrapper}>
-              <Image
-                src='https://picsum.photos/500/300/?image=10'
-                width={500}
-                height={300}
-                alt='Picture of the author'
-                className={styles.card_image}
-              />{' '}
-            </div>
-            <div className='card_content'>
-              <h3
-                className={`
-                  ${styles.card_title}
-                   ${rightGrotesk.className}`}
-              >
-                Card Grid Layout
-              </h3>{' '}
-              <p className={spaceMono.className}>
-                Demo of pixel perfect pure CSS simple responsive card grid
-                layout
-              </p>
-              <button className='btn card_btn'>Read More</button>
-            </div>
-          </div>
-          <div className={styles.horizontalElement} ref={elementHorizontal2}>
-            <div className={styles.image_wrapper}>
-              <Image
-                src='https://picsum.photos/500/300/?image=10'
-                width={500}
-                height={300}
-                alt='Picture of the author'
-                className={styles.card_image}
-              />{' '}
-            </div>
-            <div className='card_content'>
-              <h3
-                className={`
-                  ${styles.card_title}
-                   ${rightGrotesk.className}`}
-              >
-                Card Grid Layout
-              </h3>{' '}
-              <p className={spaceMono.className}>
-                Demo of pixel perfect pure CSS simple responsive card grid
-                layout
-              </p>
-              <button className='btn card_btn'>Read More</button>
-            </div>
-          </div>
-          <div className={styles.horizontalElement} ref={elementHorizontal3}>
-            <div className={styles.image_wrapper}>
-              <Image
-                src='https://picsum.photos/500/300/?image=10'
-                width={500}
-                height={300}
-                alt='Picture of the author'
-                className={styles.card_image}
-              />{' '}
-            </div>
-            <div className='card_content'>
-              <h3
-                className={`
-                  ${styles.card_title}
-                   ${rightGrotesk.className}`}
-              >
-                Card Grid Layout
-              </h3>{' '}
-              <p className={spaceMono.className}>
-                Demo of pixel perfect pure CSS simple responsive card grid
-                layout
-              </p>
-              <button className='btn card_btn'>Read More</button>
-            </div>
-          </div>
-          <div className={styles.horizontalElement} ref={elementHorizontal4}>
-            <div className={styles.image_wrapper}>
-              <Image
-                src='https://picsum.photos/500/300/?image=10'
-                width={500}
-                height={300}
-                alt='Picture of the author'
-                className={styles.card_image}
-              />{' '}
-            </div>
-            <div className='card_content'>
-              <h3
-                className={`
-                  ${styles.card_title}
-                   ${rightGrotesk.className}`}
-              >
-                Card Grid Layout
-              </h3>{' '}
-              <p className={spaceMono.className}>
-                Demo of pixel perfect pure CSS simple responsive card grid
-                layout
-              </p>
-              <button className='btn card_btn'>Read More</button>
-            </div>
-          </div>
-          <div className={styles.horizontalElement} ref={elementHorizontal5}>
-            <div className={styles.image_wrapper}>
-              <Image
-                src='https://picsum.photos/500/300/?image=10'
-                width={500}
-                height={300}
-                alt='Picture of the author'
-                className={styles.card_image}
-              />{' '}
-            </div>
-            <div className='card_content'>
-              <h3
-                className={`
-                  ${styles.card_title}
-                   ${rightGrotesk.className}`}
-              >
-                Card Grid Layout
-              </h3>{' '}
-              <p className={spaceMono.className}>
-                Demo of pixel perfect pure CSS simple responsive card grid
-                layout
-              </p>
-              <button className='btn card_btn'>Read More</button>
-            </div>
-          </div>
+          ))}
         </div>
       </section>
     </div>
--- src/app/page.js
+++ src/app/page.js
@@ -1,19 +1,27 @@
 'use client';
+// ==================== IMPORT BIBLIOTHEQUES ====================
 import { useState, useEffect, useLayoutEffect } from 'react';
 import { gsap } from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
 import Lenis from '@studio-freight/lenis';
+// ===============================================
+
+// ==================== IMPORT COMPOSANTS ====================
 import Presentation from './components/Presentation/Presentation';
 import Projects from './components/Projects/Projects';
 import Contact from './components/Contact/Contact';
+// ===============================================
 
+// ==================== STYLE ====================
 import styles from './page.module.scss'; // Importez votre fichier de style
+// ===============================================
 
 const useIsomorphicLayoutEffect =
   typeof window !== 'undefined' ? useLayoutEffect : useEffect;
 
 const lenis = typeof window !== 'undefined' ? new Lenis() : null;
 
+//!==================== COMPOSANT ====================
 export default function Index() {
   // ==================== LENIS ====================
   if (typeof window !== 'undefined') {
