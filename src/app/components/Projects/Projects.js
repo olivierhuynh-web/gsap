84813dd2adad70355e5be50efdd7dcfbb365a705
olivierhuynh-web <olivier.huynh@yahoo.fr>
2024-09-17 14:10:44 +0200

    annulation de l'animation en version mobile

--- src/app/components/Projects/Projects.js
+++ src/app/components/Projects/Projects.js
@@ -1,3 +1,4 @@
+'use client';
 import { React, useState, useEffect, useRef } from 'react';
 import Image from 'next/image';
 import { gsap } from 'gsap';
@@ -10,6 +11,7 @@ import styles from './Projects.module.scss';
 import CustomFont from '@next/font/local';
 import { Space_Mono } from '@next/font/google';
 
+// ==================== IMPORT CUSTOM FONTS ====================
 const rightGrotesk = CustomFont({
   src: '../../fonts/PPRightGrotesk-CompactDark.otf',
 });
@@ -17,109 +19,95 @@ const spaceMono = Space_Mono({
   subsets: ['latin'],
   weight: ['400'],
 });
+// ===============================================
 
+// ==================== INITIALISATION DES RÉFÉRENCES ====================
 const Projects = () => {
   const section2Ref = useRef(null);
   const section2WrapperRef = useRef(null);
 
-  const [isSmallScreen, setIsSmallScreen] = useState(0);
+  // ==================== INITIALISATION DE L'ÉTAT ====================
+  const [isSmallScreen, setIsSmallScreen] = useState(true);
 
+  // ==================== GESTION DE LA TAILLE DE L'ÉCRAN ====================
   useEffect(() => {
-    const checkScreenSize = () => {
-      setIsSmallScreen(window.innerWidth < 1000);
-    };
+    if (typeof window !== 'undefined') {
+      // Fonction pour vérifier la taille de l'écran
+      const checkScreenSize = () => {
+        const isSmall = window.innerWidth < 1000;
+        console.log(
+          'Current width:',
+          window.innerWidth,
+          'isSmallScreen:',
+          isSmall
+        );
+        setIsSmallScreen(isSmall);
+      };
 
-    window.addEventListener('resize', checkScreenSize);
+      // Initialiser la taille de l'écran lors du montage
+      checkScreenSize();
 
-    return () => {
-      window.removeEventListener('resize', checkScreenSize);
-    };
+      // Ajouter un écouteur d'événements pour redimensionnement de la fenêtre
+      window.addEventListener('resize', checkScreenSize);
+
+      // Nettoyer l'écouteur d'événements
+      return () => {
+        window.removeEventListener('resize', checkScreenSize);
+      };
+    }
   }, []);
 
+  // ==================== APPLICATION DES ANIMATIONS ====================
   useEffect(() => {
-    const cards = gsap.utils.toArray(`.${styles['section2__card']}`);
+    if (!isSmallScreen) {
+      // Appliquer les animations seulement si l'écran est grand
+      const cards = gsap.utils.toArray(`.${styles['section2__card']}`);
 
-    gsap.registerPlugin(ScrollTrigger);
-    const tl = gsap.timeline();
+      gsap.registerPlugin(ScrollTrigger);
+      const tl = gsap.timeline();
 
-    tl.add(introductionCards(section2WrapperRef.current, section2Ref.current));
-    tl.add(scaleCards(cards));
-    tl.add(pin(section2WrapperRef.current, section2Ref.current));
-  }, []);
+      tl.add(
+        introductionCards(section2WrapperRef.current, section2Ref.current)
+      );
+      tl.add(scaleCards(cards));
+      tl.add(pin(section2WrapperRef.current, section2Ref.current));
+    } else {
+      console.log('Animations skipped for small screen');
+    }
+  }, [isSmallScreen]);
 
+  // ==================== RENDU DU COMPOSANT ====================
   return (
     <section className={styles['section2']} ref={section2Ref} id='projets'>
-      {isSmallScreen ? (
-        <div className={styles['section2__wrapper']} ref={section2WrapperRef}>
-          {data.projects.map((project) => (
-            <div className={styles['section2__wrapper__card']} key={project.id}>
-              <div className={styles['section2__wrapper__card__image-wrapper']}>
-                <div
-                  className={styles['section2__wrapper__card__image-wrapper']}
-                >
-                  <Image
-                    src='https://picsum.photos/500/300/?image=10'
-                    alt='Picture of the author'
-                    width={500} // Largeur de l'image
-                    height={100} // Hauteur de l'image
-                    className={
-                      styles[
-                        'section2__wrapper__card__image-wrapper__card-image'
-                      ]
-                    } // Assure-toi que la classe CSS est correcte
-                    // style={{
-                    //   objectFit: 'contain', // Assure que l'image garde ses proportions
-                    //   objectPosition: 'top left', // Positionne l'image en haut à gauche
-                    // }}
-                  />
-                </div>
-              </div>
-              <div className={styles['section2__wrapper__card__card-content']}>
-                <h3
-                  className={`${styles['section2__wrapper__card-title']} ${rightGrotesk.className}`}
-                >
-                  {project.name}
-                </h3>
-                <p
-                  className={`${styles['section2__wrapper__card-description']} ${spaceMono.className}`}
-                >
-                  {project.description}
-                </p>
-              </div>
+      <div className={styles['section2__wrapper']} ref={section2WrapperRef}>
+        {data.projects.map((project) => (
+          <div className={styles['section2__wrapper__card']} key={project.id}>
+            <div className={styles['section2__wrapper__card__image-wrapper']}>
+              <Image
+                src='https://picsum.photos/500/300/?image=10'
+                alt='Picture of the author'
+                width={500} // Largeur de l'image
+                height={isSmallScreen ? 100 : 300} // Hauteur de l'image
+                className={
+                  styles['section2__wrapper__card__image-wrapper__card-image']
+                } // Assure-toi que la classe CSS est correcte
+              />
             </div>
-          ))}
-        </div>
-      ) : (
-        <div className={styles['section2__wrapper']} ref={section2WrapperRef}>
-          {data.projects.map((project) => (
-            <div className={styles['section2__wrapper__card']} key={project.id}>
-              <div className={styles['section2__wrapper__card__image-wrapper']}>
-                <Image
-                  src='https://picsum.photos/500/300/?image=10'
-                  width={500}
-                  height={300}
-                  alt='Picture of the author'
-                  className={
-                    styles['section2__wrapper__card__image-wrapper__card-image']
-                  }
-                />
-              </div>
-              <div className={styles['section2__wrapper__card__card-content']}>
-                <h3
-                  className={`${styles['section2__wrapper__card-title']} ${rightGrotesk.className}`}
-                >
-                  {project.name}
-                </h3>
-                <p
-                  className={`${styles['section2__wrapper__card-description']} ${spaceMono.className}`}
-                >
-                  {project.description}
-                </p>
-              </div>
+            <div className={styles['section2__wrapper__card__card-content']}>
+              <h3
+                className={`${styles['section2__wrapper__card-title']} ${rightGrotesk.className}`}
+              >
+                {project.name}
+              </h3>
+              <p
+                className={`${styles['section2__wrapper__card-description']} ${spaceMono.className}`}
+              >
+                {project.description}
+              </p>
             </div>
-          ))}
-        </div>
-      )}
+          </div>
+        ))}
+      </div>
     </section>
   );
 };
