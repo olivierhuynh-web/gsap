3c1aaa31520e3542a151ebd2251d9a6211480aeb
olivierhuynh-web <olivier.huynh@yahoo.fr>
2024-09-17 15:39:24 +0200

    fini le décalage de contact

--- src/app/components/Contact/Contact.js
+++ src/app/components/Contact/Contact.js
@@ -17,36 +17,48 @@ const inter = Inter({
 import { gsap } from 'gsap';
 
 const Contact = ({ timeline }) => {
-  const [isHovered, setIsHovered] = useState(false);
+  const [isSmallScreen, setIsSmallScreen] = useState(false);
 
   const section3 = useRef(null);
   const emailBubble = useRef(null);
   const contactLink = useRef(null);
   const blueDot = useRef(null);
   const socialBubble = useRef(null);
-  // const horizontalBar = useRef(null);
-  // const verticalBar = useRef(null);
-  // const link = useRef(null);
+
+  // Fonction pour vérifier la taille de l'écran
+  const checkScreenSize = () => {
+    setIsSmallScreen(window.innerWidth < 1000);
+  };
+
+  useEffect(() => {
+    checkScreenSize(); // Vérifiez la taille de l'écran lors du montage
+    window.addEventListener('resize', checkScreenSize); // Écouter les changements de taille de l'écran
+    return () => {
+      window.removeEventListener('resize', checkScreenSize); // Nettoyer l'écouteur d'événements
+    };
+  }, []);
 
   useEffect(() => {
     if (timeline) {
-      // contactLinkToGreen(contactLink.current);
-
       timeline.add(
-        slideEmail(emailBubble.current, section3.current, contactLink.current),
+        // Appliquer slideEmail uniquement si l'écran est suffisamment large
+        !isSmallScreen &&
+          slideEmail(
+            emailBubble.current,
+            section3.current,
+            contactLink.current
+          ),
         slideSocial(socialBubble.current, section3.current),
         toBlue(blueDot.current, section3.current)
       );
     }
-  }, [timeline]);
+  }, [timeline, isSmallScreen]);
 
   return (
-    // <div>
     <section className={styles.section3} ref={section3} id='contact'>
       <div className={styles.section3_wrapper}>
         <div className={styles.contactContainer}>
           <div className={`${spaceMono.className} `}>
-            {' '}
             <span className={styles.bluedot} ref={blueDot}>
               ●{' '}
             </span>
@@ -73,7 +85,6 @@ const Contact = ({ timeline }) => {
         </div> */}
       </div>
     </section>
-    // </div>
   );
 };
 
